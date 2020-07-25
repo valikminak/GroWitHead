a67bfb40a2fe16a127a3549f7df0ed565725d9d5
Valentyn Minakov <valikminak@gmail.com>
2020-07-25 18:21:22 +0300

    Updates

---
+++ precache-manifest.2223811a4f0a18dba7170c35c41eba0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa711eae8597705d0cc3e09878b4eaa",
+    "url": "/GroWitHead/index.html"
+  },
+  {
+    "revision": "51b0c85378b7ee631d9b",
+    "url": "/GroWitHead/static/css/main.5b2511f0.chunk.css"
+  },
+  {
+    "revision": "3d98fb66516414b27da3",
+    "url": "/GroWitHead/static/js/2.4b9b3e79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GroWitHead/static/js/2.4b9b3e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51b0c85378b7ee631d9b",
+    "url": "/GroWitHead/static/js/main.a2d606bd.chunk.js"
+  },
+  {
+    "revision": "403dfa0ab24fa2b8a02b",
+    "url": "/GroWitHead/static/js/runtime-main.d382324b.js"
+  }
+]);
